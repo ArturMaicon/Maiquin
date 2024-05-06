2888a72194f4c4ff70311108462f24ac2dd20ae4
ArturMaicon <169092702+ArturMaicon@users.noreply.github.com>
2024-05-06 18:54:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var name = prompt("Qual o seu nome?")
+var age = prompt("Qual o seu nome?")
+
+let Artur = {
+nome = "Artur",
+age = 19,
+isHuman = true
+}
+
+
+console.log(`Olá, ${Artur.nome}, você tem ${Artur.age} anos`)
